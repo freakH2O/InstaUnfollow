4a7b2757068c04b81a5f2db50c4a160a6c76b3c9
Hamza Hassan <hassanhamza930@gmail.com>
2019-04-15 23:34:25 +0500

    Update InstaUnfollow.py

--- InstaUnfollow.py
+++ InstaUnfollow.py
@@ -9,6 +9,15 @@ from selenium.webdriver.common.by import By
 import random
 
 
+def tryy(x):
+    relay=True
+    while relay==True:
+        try:
+            x.click()
+            relay=False
+        except Exception:
+            continue
+
 options = Options()
 options.add_argument("start-maximized")
 options.add_argument("disable-infobars")
@@ -16,70 +25,10 @@ options.add_argument("--disable-notifications")
 options.add_argument('user-data-dir= selenium')
 browser = webdriver.Chrome(chrome_options=options, executable_path=r'chromedriver.exe')
 
-
-browser.get('https://www.instagram.com/accounts/login/')
-
-#---------------------------------------------------------------------------------------------------------------------
-
-
-def waituntilclickable(element,time):
-    i=0
-    while i<time:
-        try:
-            time.sleep(1)
-            element.click()
-            i=time
-            i+=1
-        except Exception:
-            i+=1
-            continue
-
-#---------------------------------------------------------------------------------------------------------------------
-
-
-relay=True
-while relay==True:
-   if browser.current_url == 'https://www.instagram.com/':
-       browser.get('https://www.instagram.com/wear.horizon')
-       break
-
-
-
-try:
-    login = browser.find_element_by_name('username')
-    waituntilclickable(login,5)
-    login.send_keys('wear.horizon', Keys.ENTER)
-    password = browser.find_element_by_name('password')
-    waituntilclickable(password, 5)
-    password.send_keys('dcanoslimshady', Keys.ENTER)
-
-    relay = True
-    while relay == True:
-        if browser.current_url == 'https://www.instagram.com/':
-            browser.get('https://www.instagram.com/meme____machine___/')
-            relay=False
-except Exception:
-    relay=True
-
-
-#---------------------------------------------------------------------------------------------------------------------
-
-relay=True
-
-while relay==True:
-    try:
-      try:
-        time.sleep(random.randint(3,10))
-        following = browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a/span').click()
-        time.sleep(2)
-        browser.find_element_by_tag_name('html').send_keys(Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.TAB,Keys.SPACE)
-        time.sleep(1)
-        browser.find_element_by_tag_name('html').send_keys(Keys.TAB, Keys.SPACE)
-        time.sleep(1)
-        time.sleep(random.randint(3, 10))
-        browser.refresh()
-      except Exception:
-          continue
-    except Exception:
-        continue
-
+while True:
+    browser.get('https://www.instagram.com/ring_one_pk/')
+    tryy(browser.find_element_by_xpath('/html/body/span/section/main/div/header/section/ul/li[3]/a'))
+    time.sleep(2)
+    tryy(browser.find_element_by_xpath('/html/body/div[3]/div/div[2]/ul/div/li[1]/div/div[3]/button'))
+    tryy(browser.find_element_by_xpath('/html/body/div[4]/div/div/div[3]/button[1]'))
+    time.sleep(random.randint(10,15))

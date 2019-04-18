2fd032a8c23d2f3e3238333bf82519750cd99cae
Hamza Hassan <hassanhamza930@gmail.com>
2019-04-18 23:38:24 +0500

    Update InstaUnfollow.py

--- InstaUnfollow.py
+++ InstaUnfollow.py
@@ -30,6 +30,7 @@ while True:
     browser.get('https://www.instagram.com')
     time.sleep(2)
     tryy(browser.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[3]/a/span'))
+    time.sleep(2)
     tryy(browser.find_element_by_xpath('/html/body/span/section/main/div/header/section/ul/li[3]/a'))
     time.sleep(2)
     tryy(browser.find_element_by_xpath('/html/body/div[3]/div/div[2]/ul/div/li[1]/div/div[3]/button'))
